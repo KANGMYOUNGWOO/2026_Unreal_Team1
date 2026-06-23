1c82e053060ab52a3d1d76c060848de3b09f5062
DESKTOP-JV9Q3R0\y8268 <y826855@naver.com>
2026-06-23 14:34:14 +0900

    feat : 범퍼 베이스 클래스 구성, 범퍼 데이터 struct 구성  - 범퍼에 필요한 기능 위주로 먼저 구현함  - 사용될 타입을 미리 enum으로 나눠둠

---
+++ Source/PinBallLike/Actor/Bumper/PBBumperBase.cpp
@@ -0,0 +1,98 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PBBumperBase.h"
+
+#include "PinBallLike/Actor/Ball/BallBase.h"
+
+APBBumperBase::APBBumperBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void APBBumperBase::AddTriggerCount(ABallBase* Ball, const int32 Amount)
+{
+	if (!IsValid(Ball) || !CanAccumulateTrigger() || Amount <= 0)
+	{
+		return;
+	}
+
+	const int32 RequiredTriggerCount = FMath::Max(BumperData.RequiredTriggerCount, 1);
+	CurrentTriggerCount = FMath::Clamp(
+		CurrentTriggerCount + Amount,
+		0,
+		RequiredTriggerCount);
+	OnTriggerCountChanged(CurrentTriggerCount, RequiredTriggerCount);
+
+	const bool IsAlreadyReady = IsReady;
+	IsReady = CurrentTriggerCount >= RequiredTriggerCount;
+	if (IsReady && !IsAlreadyReady)
+	{
+		OnBumperReady();
+	}
+
+	if (CanActivate())
+	{
+		ActivateBumper(Ball);
+	}
+}
+
+void APBBumperBase::ActivateBumper(ABallBase* Ball)
+{
+	if (!CanActivate())
+	{
+		return;
+	}
+
+	IsActivating = true;
+	IsReady = false;
+	ResetTriggerCount();
+
+	OnBumperActivated(Ball);
+	ApplyBumperEffect(Ball);
+}
+
+void APBBumperBase::FinishActivation()
+{
+	if (!IsActivating)
+	{
+		return;
+	}
+
+	IsActivating = false;
+	OnBumperFinished();
+}
+
+void APBBumperBase::ResetTriggerCount()
+{
+	CurrentTriggerCount = 0;
+	IsReady = false;
+	OnTriggerCountChanged(CurrentTriggerCount, FMath::Max(BumperData.RequiredTriggerCount, 1));
+}
+
+void APBBumperBase::SetIsEnabled(const bool IsNewEnabled)
+{
+	IsEnabled = IsNewEnabled;
+	if (!IsEnabled)
+	{
+		ResetTriggerCount();
+		IsActivating = false;
+	}
+}
+
+bool APBBumperBase::CanActivate() const
+{
+	return IsEnabled
+		&& !IsActivating
+		&& CurrentTriggerCount >= FMath::Max(BumperData.RequiredTriggerCount, 1);
+}
+
+bool APBBumperBase::CanAccumulateTrigger() const
+{
+	return IsEnabled && !IsActivating;
+}
+
+void APBBumperBase::ApplyBumperEffect_Implementation(ABallBase* Ball)
+{
+	FinishActivation();
+}
