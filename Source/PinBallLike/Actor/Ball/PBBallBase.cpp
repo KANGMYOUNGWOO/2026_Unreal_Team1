af9c2994d45f114a15ae22145bb836fe8cd634ad
dotori <a01062688790@gmail.com>
2026-06-26 16:58:31 +0900

    hotfix : dev불일치 고침

--- Source/PinBallLike/Actor/Ball/PBBallBase.cpp
+++ Source/PinBallLike/Actor/Ball/PBBallBase.cpp
@@ -66,15 +66,10 @@ void APBBallBase::ApplyStatData(const TArray<FPBBallStatData>& StatData)
 		{
 			continue;
 		}
-
-		if (StatComponent->HasStat(Stat.StatName))
-		{
-			continue;
-		}
-
 		StatComponent->SetStat(Stat.StatName, Stat.Value);
 	}
 }
+
 
 void APBBallBase::ApplyResourceData(const TArray<FPBBallResourceData>& ResourceData)
 {
