845888728bfe6a67d6cd6f48f07cd7c17069ebc2
cbs <y826855@naver.com>
2026-06-29 15:52:55 +0900

    feat : 터렛 projectile 구성  - projectile 인터페이스 추가  - 터렛 발사 컴포넌트 추가

---
+++ Source/PinBallLike/Actor/Bumper/Component/PBTurretFireComponent.h
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PBTurretFireComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(
+	FPBTurretProjectileSignature,
+	AActor*, Projectile);
+
+UCLASS(ClassGroup = (PinBall), meta = (BlueprintSpawnableComponent))
+class PINBALLLIKE_API UPBTurretFireComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UPBTurretFireComponent();
+
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Bumper|Turret|Fire")
+	AActor* FireOnce();
+
+	UFUNCTION(BlueprintCallable, Category = "Bumper|Turret|Pool")
+	void ReleaseProjectile(AActor* Projectile);
+
+	UPROPERTY(BlueprintAssignable, Category = "Bumper|Turret|Fire")
+	FPBTurretProjectileSignature OnTurretProjectileFired;
+
+	UPROPERTY(BlueprintAssignable, Category = "Bumper|Turret|Fire")
+	FPBTurretProjectileSignature OnTurretProjectileActivated;
+
+	UPROPERTY(BlueprintAssignable, Category = "Bumper|Turret|Fire")
+	FPBTurretProjectileSignature OnTurretProjectileDeactivated;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bumper|Turret|Fire")
+	TSubclassOf<AActor> ProjectileClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bumper|Turret|Fire")
+	FName MuzzleTag = TEXT("TurretMuzzle");
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bumper|Turret|Fire", meta = (ClampMin = "0.0"))
+	float ProjectileLifeTime = 2.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bumper|Turret|Pool")
+	bool IsUseObjectPool = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bumper|Turret|Pool", meta = (ClampMin = "0"))
+	int32 InitialPoolSize = 5;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bumper|Turret|Pool", meta = (ClampMin = "1"))
+	int32 MaxPoolSize = 20;
+
+private:
+	FTransform GetMuzzleTransform() const;
+	AActor* GetProjectileFromPool();
+	AActor* SpawnProjectileActor();
+	void ActivateProjectile(AActor* Projectile, const FTransform& SpawnTransform);
+	void DeactivateProjectile(AActor* Projectile);
+	void ClearPool();
+
+	UPROPERTY()
+	TArray<TObjectPtr<AActor>> PooledProjectiles;
+
+	UPROPERTY()
+	TArray<TObjectPtr<AActor>> ActiveProjectiles;
+
+	TMap<TWeakObjectPtr<AActor>, FTimerHandle> ProjectileLifeTimerHandles;
+};
