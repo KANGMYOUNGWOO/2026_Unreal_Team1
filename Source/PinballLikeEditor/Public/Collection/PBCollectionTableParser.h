02ea5c48cabb4431c1ff50614cf58eeaa9ef9d2d
liomwh-wyatt <liomwh@gmail.com>
2026-07-07 20:56:51 +0900

    docs: 도감 파서 export 시트 기준 설명 수정

--- Source/PinballLikeEditor/Public/Collection/PBCollectionTableParser.h
+++ Source/PinballLikeEditor/Public/Collection/PBCollectionTableParser.h
@@ -5,7 +5,8 @@
 #include "PBCollectionTableParser.generated.h"
 
 /**
- * Collection_Master 시트를 도감 DataTable 행으로 변환하는 에디터 전용 파서입니다.
+ * DT_Collection_Export 시트를 도감 DataTable 행으로 변환하는 에디터 전용 파서입니다.
+ * Collection_Master는 사람이 관리하는 허브 탭이고, 이 파서는 언리얼로 내보낼 최종 헤더만 읽습니다.
  * 런타임 도감은 이 파서가 채운 FPBCollectionTableRow 데이터를 읽어 목록 UI를 구성합니다.
  */
 UCLASS()
