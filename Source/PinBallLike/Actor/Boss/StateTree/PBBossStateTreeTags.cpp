37d0945ee098e025942ab2edc9d3c688efe3f83f
dotori <a01062688790@gmail.com>
2026-07-07 09:54:55 +0900

    chore : state tree tag 컨벤션에 맞게 수정.

--- Source/PinBallLike/Actor/Boss/StateTree/PBBossStateTreeTags.cpp
+++ Source/PinBallLike/Actor/Boss/StateTree/PBBossStateTreeTags.cpp
@@ -2,9 +2,10 @@
 
 namespace PBBossStateTreeTags
 {
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(RequestIdle, "Boss.State.Request.Idle", "Requests the boss StateTree to enter idle state.");
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(RequestPattern, "Boss.State.Request.Pattern", "Requests the boss StateTree to enter pattern state.");
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(RequestGroggy, "Boss.State.Request.Groggy", "Requests the boss StateTree to enter groggy state.");
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(RequestEnraged, "Boss.State.Request.Enraged", "Requests the boss StateTree to enter enraged state.");
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(RequestDead, "Boss.State.Request.Dead", "Requests the boss StateTree to enter dead state.");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(RequestIdle, "Event.Boss.State.Idle", "Requests the boss StateTree to enter idle state.");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(RequestPattern, "Event.Boss.State.Pattern", "Requests the boss StateTree to enter pattern state.");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(RequestGroggy, "Event.Boss.State.Groggy", "Requests the boss StateTree to enter groggy state.");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(RequestEnraged, "Event.Boss.State.Enraged", "Requests the boss StateTree to enter enraged state.");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(RequestDead, "Event.Boss.State.Dead", "Requests the boss StateTree to enter dead state.");
 }
+//Event.UI.Deck.Drag.Started
