b344fd48af3694a91e79ecb579484d53e3548724
cbs <y826855@naver.com>
2026-07-14 17:43:26 +0900

    feat : 레이저 스킬 구현

---
+++ Source/PinBallLike/Actor/Ball/Skill/PBLaserSkillActor.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PBBallSkillActorBase.h"
+#include "PBLaserSkillActor.generated.h"
+
+class APBBallBase;
+class UBoxComponent;
+class USceneComponent;
+class UPBTimedAreaDamageComponent;
+
+UCLASS(Blueprintable)
+class PINBALLLIKE_API APBLaserSkillActor : public APBBallSkillActorBase
+{
+	GENERATED_BODY()
+
+public:
+	APBLaserSkillActor();
+
+	virtual void InitializeSkill(
+		APBBallBase* InOwnerBall,
+		int32 InDamageAmount,
+		float InDuration,
+		int32 InDamageCount) override;
+
+protected:
+	virtual void Tick(float DeltaTime) override;
+	virtual void EnterActiveState() override;
+	virtual void EnterFinishingState() override;
+	virtual void EnterStoppingState() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Ball|Skill|Laser")
+	TObjectPtr<USceneComponent> Root;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Ball|Skill|Laser")
+	TObjectPtr<UBoxComponent> LaserCollision;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Ball|Skill|Laser")
+	TObjectPtr<UPBTimedAreaDamageComponent> TimedAreaDamageComponent;
+
+private:
+	void DeactivateLaser();
+	void UpdateTargetTracking();
+
+	TWeakObjectPtr<AActor> TargetActor;
+};
