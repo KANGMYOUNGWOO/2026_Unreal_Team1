cd5555ba554566cd534182dc5c965d1cc9424e89
Quanx0411 <quanx0411@gmail.com>
2026-07-15 16:24:03 +0900

    fix) tooltip

--- Source/PinBallLike/Relic/UI/PBRelicIconWidget.cpp
+++ Source/PinBallLike/Relic/UI/PBRelicIconWidget.cpp
@@ -1,8 +1,84 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "PBRelicIconWidget.h"
+
+#include "PinBallLike/Table/Relic/Struct/PBRelicTableRow.h"
+#include "PinBallLike/Subsystem/PBTableDataSubsystem.h"
+#include "PinBallLike/Relic/UI/PBRelicTooltipWidget.h"
+
 
 void UPBRelicIconWidget::SetRelicId(FName InRelicId)
 {
+	RelicId = InRelicId;
+
+	UGameInstance* GameInstance =
+		GetGameInstance();
+
+	if (!GameInstance)
+	{
+		return;
+	}
+
+	UPBTableDataSubsystem* TableSubsystem =
+		GameInstance->GetSubsystem<UPBTableDataSubsystem>();
+
+	if (!TableSubsystem)
+	{
+		return;
+	}
+
+	FPBRelicTableRow RelicRow;
+
+	if (!TableSubsystem->FindRelicRow(
+		RelicId,
+		RelicRow))
+	{
+		return;
+	}
+
+	if (!RelicTooltipWidgetClass)
+	{
+		return;
+	}
+
+	UPBRelicTooltipWidget* TooltipWidget =
+		CreateWidget<UPBRelicTooltipWidget>(
+			GetOwningPlayer(),
+			RelicTooltipWidgetClass);
+
+	if (!TooltipWidget)
+	{
+		return;
+	}
+
+	TooltipWidget->SetRelicData(
+		RelicRow.DisplayName,
+		RelicRow.Description);
+
+	SetToolTip(
+		TooltipWidget);
+}
+
+void UPBRelicIconWidget::NativeOnMouseEnter(
+	const FGeometry& InGeometry,
+	const FPointerEvent& InMouseEvent)
+{
+	Super::NativeOnMouseEnter(InGeometry, InMouseEvent);
+
+	UE_LOG(
+		LogTemp,
+		Warning,
+		TEXT("[RelicIcon] Mouse Enter. RelicId=%s"),
+		*RelicId.ToString());
+}
+
+void UPBRelicIconWidget::NativeOnMouseLeave(
+	const FPointerEvent& InMouseEvent)
+{
+	Super::NativeOnMouseLeave(InMouseEvent);
+
+	UE_LOG(
+		LogTemp,
+		Warning,
+		TEXT("[RelicIcon] Mouse Leave. RelicId=%s"),
+		*RelicId.ToString());
 }
