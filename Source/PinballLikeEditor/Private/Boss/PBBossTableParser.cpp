91f0fa635798980ada7f73c67ca8263fc5f75f82
dotori <a01062688790@gmail.com>
2026-07-16 10:41:40 +0900

    feat : 파서경로수색 변경

--- Source/PinballLikeEditor/Private/Boss/PBBossTableParser.cpp
+++ Source/PinballLikeEditor/Private/Boss/PBBossTableParser.cpp
@@ -4,7 +4,9 @@
 #include "Boss/PBBossTableParser.h"
 
 #include "PBSheetParserUtils.h"
+#include "AssetRegistry/AssetRegistryModule.h"
 #include "Camera/CameraShakeBase.h"
+#include "Modules/ModuleManager.h"
 #include "PinBallLike/Actor/Boss/PBBossBase.h"
 #include "PinBallLike/Actor/Boss/UI/PBBossUILayerWidget.h"
 #include "PinBallLike/Table/Boss/DataAsset/PBBossDataAsset.h"
@@ -140,18 +142,30 @@ UPBBossDataAsset* UPBBossTableParser::SetupBossDataAsset(
 
 TSoftClassPtr<APBBossBase> UPBBossTableParser::FindBossClass(const FName BossClassId) const
 {
-	for (const FPBSheetAssetPathPreset& BossClassPreset : BossClassPresets)
+	if (BossClassId.IsNone())
 	{
-		if (!BossClassPreset.IsValid())
+		return nullptr;
+	}
+
+	const FName BossAssetName(*FString::Printf(TEXT("BP_%s"), *BossClassId.ToString()));
+	FAssetRegistryModule& AssetRegistryModule =
+		FModuleManager::LoadModuleChecked<FAssetRegistryModule>(TEXT("AssetRegistry"));
+
+	FARFilter Filter;
+	Filter.PackagePaths.Add(TEXT("/Game/Blueprints/Boss"));
+	Filter.bRecursivePaths = true;
+
+	TArray<FAssetData> AssetDatas;
+	AssetRegistryModule.Get().GetAssets(Filter, AssetDatas);
+	for (const FAssetData& AssetData : AssetDatas)
+	{
+		if (AssetData.AssetName != BossAssetName)
 		{
 			continue;
 		}
 
-		TSoftClassPtr<APBBossBase> BossClass = FindBlueprintClass<APBBossBase>(BossClassPreset, BossClassId);
-		if (!BossClass.IsNull())
-		{
-			return BossClass;
-		}
+		const FString ClassPath = FString::Printf(TEXT("%s_C"), *AssetData.GetSoftObjectPath().ToString());
+		return TSoftClassPtr<APBBossBase>(FSoftObjectPath(ClassPath));
 	}
 
 	return nullptr;
