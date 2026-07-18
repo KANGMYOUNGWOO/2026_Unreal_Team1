fdf2a6098bca90d5c7fdca88009f93b7eaba9e51
liomwh-wyatt <liomwh@gmail.com>
2026-07-19 08:06:41 +0900

    chore: 범퍼 코드의 불필요한 Unreal 기본 주석 정리
    범퍼 효과와 영역 클래스에 남아 있던 기본 자리표시자 주석만 제거합니다.
    
    실행 로직은 변경하지 않으며 Epic 저작권 주석은 유지합니다.

--- Source/PinBallLike/Actor/Bumper/Effect/PBComboBumperEffect.cpp
+++ Source/PinBallLike/Actor/Bumper/Effect/PBComboBumperEffect.cpp
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "PBComboBumperEffect.h"
 
 #include "PinBallLike/Actor/Bumper/Modular/PBModularBumperBase.h"
--- Source/PinBallLike/Actor/Bumper/Effect/PBVelocityBoostBumperEffect.cpp
+++ Source/PinBallLike/Actor/Bumper/Effect/PBVelocityBoostBumperEffect.cpp
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "PBVelocityBoostBumperEffect.h"
 
 #include "PinBallLike/Actor/Bumper/Modular/PBModularBumperBase.h"
